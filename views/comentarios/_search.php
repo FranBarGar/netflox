80d1e2bf3a7cf9794e44649e21062bbe00b93175
Francisco Barba <francisco.barba@iesdonana.org>
2019-06-08 21:01:29 +0200

    [ADD] Busqueda por diferentes parametros

--- views/comentarios/_search.php
+++ views/comentarios/_search.php
@@ -17,6 +17,9 @@ $js = <<<EOJS
     $('#krajee-sucks').on('change', (e) => {
        $('#comentariossearch-valoracion').val(e.target.value);
     });
+    $('#search-switch-button').on('click', (e) => {
+        $('#search-maximo').toggle(750);
+    });
 EOJS;
 
 $this->registerJs($js);
@@ -24,64 +27,75 @@ $this->registerJs($js);
 
 <div class="row all-comments comentarios-order">
 
-    <div class="col-md-12">
-        <?php $form = ActiveForm::begin([
-            'action' => ['index'],
-            'method' => 'get',
-        ]); ?>
+    <?php $form = ActiveForm::begin([
+        'action' => ['index'],
+        'method' => 'get',
+    ]); ?>
 
-        <?= $form->field($model, 'cuerpo') ?>
-        <?= $form->field($model, 'usuario_id') ?>
-
+    <div class="form-group col-md-6">
+        <?= $form->field($model, 'literalTitulo')->label('Show') ?>
     </div>
-    <div class="row">
-        <div class="col-md-12">
-            <div class="form-group col-md-6">
-                <label for="krajee-sucks">Valoracion</label>
-                <input type="text" id="krajee-sucks" class="form-control">
-                <?=
-                $form->field($model, 'valoracion')->hiddenInput()->label(false);
-                ?>
-            </div>
 
-            <div class="form-group col-md-6">
-                <?= $form->field($model, 'show_id') ?>
+    <div class="form-group col-md-6">
+        <?= $form->field($model, 'literalUsuario')->label('Usuario') ?>
+    </div>
+
+    <div id="search-buttons" class="form-group col-md-12">
+        <?= Html::submitButton('Buscar', ['class' => 'btn btn-primary col-md-3']) ?>
+        <?=
+        Html::button(
+            '<span class="glyphicon glyphicon-cog"></span>',
+            [
+                'class' => 'btn btn-danger col-md-1',
+                'id' => 'search-switch-button'
+            ]
+        )
+        ?>
+    </div>
+
+    <div id="search-maximo" style="display: none">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="form-group col-md-6">
+                    <?= $form->field($model, 'cuerpo')->textarea(['rows' => 5]) ?>
+                </div>
+
+                <div class="form-group col-md-6">
+                    <label for="krajee-sucks">Valoracion</label>
+                    <input type="text" id="krajee-sucks" class="form-control">
+                    <?=
+                    $form->field($model, 'valoracion')->hiddenInput()->label(false);
+                    ?>
+                </div>
             </div>
         </div>
-    </div>
-    <div class="form-group col-md-6">
-        <?=
-        $form->field($model, 'orderBy')
-            ->widget(Select2::class, [
-                'data' => Comentarios::ORDER_BY,
-                'options' => [
-                    'placeholder' => 'Seleccione el tipo de ordenación...',
-                ],
-                'pluginOptions' => [
-                    'allowClear' => true,
-                ],
-            ]);
-        ?>
-    </div>
-    <div class="form-group col-md-6">
-        <?=
-        $form->field($model, 'orderType')
-            ->widget(Select2::class, [
-                'data' => Utility::ORDER_TYPE,
-                'options' => [
-                    'placeholder' => 'Selecciona un tipo de show a buscar...',
-                ],
-                'pluginOptions' => [
-                    'allowClear' => true
-                ],
-            ]);
-        ?>
-    </div>
-
-    <div class="col-md-12">
-        <div class="form-group">
-            <?= Html::submitButton('Search', ['class' => 'btn btn-primary']) ?>
-            <?= Html::resetButton('Reset', ['class' => 'btn btn-outline-secondary']) ?>
+        <div class="form-group col-md-6">
+            <?=
+            $form->field($model, 'orderBy')
+                ->widget(Select2::class, [
+                    'data' => Comentarios::ORDER_BY,
+                    'options' => [
+                        'placeholder' => 'Seleccione el tipo de ordenación...',
+                    ],
+                    'pluginOptions' => [
+                        'allowClear' => true,
+                    ],
+                ]);
+            ?>
+        </div>
+        <div class="form-group col-md-6">
+            <?=
+            $form->field($model, 'orderType')
+                ->widget(Select2::class, [
+                    'data' => Utility::ORDER_TYPE,
+                    'options' => [
+                        'placeholder' => 'Selecciona un tipo de show a buscar...',
+                    ],
+                    'pluginOptions' => [
+                        'allowClear' => true
+                    ],
+                ]);
+            ?>
         </div>
     </div>
 
