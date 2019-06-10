8fa775ee2c127dfc3257b237c1bdd6d1611edeac
Francisco Barba García <francisco.barba@iesdonana.org>
2019-06-10 23:33:27 +0200

    [MOD] Cambiar email

--- views/usuarios/myProfile.php
+++ views/usuarios/myProfile.php
@@ -109,11 +109,9 @@ $accionesUrl = Url::to(['usuarios-shows/get-acciones', 'UsuariosShowsSearch[usua
                 <p>Usuario desde el <?= Yii::$app->formatter->asDate($model->created_at, 'long') ?></p>
             </div>
             <h2><?= Html::encode($model->nick) ?></h2>
-            <div>
-                <label for="email">Email: </label> <?= $model->email ?>
-            </div>
             <div class="biografia-form">
                 <?php $form = ActiveForm::begin(['action' => ['usuarios/update', 'id' => Yii::$app->user->id]]); ?>
+                <?= $form->field($model, 'email') ?>
                 <?= $form->field($model, 'biografia')->textarea(['rows' => '9']) ?>
                 <div class="form-group">
                     <?= Html::submitButton('Guardar', ['class' => 'btn btn-block btn-primary', 'name' => 'login-button']) ?>
